490be3d91fb74420bf1813631fc99f59be9d71a7
ndhu410421304 <33059129+ndhu410421304@users.noreply.github.com>
2019-12-20 01:09:07 +0800

    modify file with style

--- Detectron2/detectron_test_mod_mult.py
+++ Detectron2/detectron_test_mod_mult.py
@@ -1,51 +1,54 @@
-import torch, torchvision
-torch.__version__
-
+import torch
+import torchvision
 import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
-
-import numpy as np
+import os
+import json
+import itertools
 import cv2
 import random
-
+import numpy as np
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
-
-import os
-import numpy as np
-import json
+from detectron2.utils.logger import setup_logger
 from detectron2.structures import BoxMode
-import itertools
+from detectron2.data import DatasetCatalog
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
 from detectron2.utils.visualizer import ColorMode
-
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
-coco = COCO("pascal_train.json") # load training annotations
-coco2 = COCO("test.json") # load training annotations
-
 from utils import binary_mask_to_rle
+from detectron2.engine import DefaultTrainer
+
+torch.__version__
+setup_logger()
+coco = COCO("pascal_train.json")  # load training annotations
+coco2 = COCO("test.json")  # load training annotations
+
 
 # use get dicts file to generate dictionary for catalog
 def get_voc_test_dicts(img_dir):
     img_ids = list(coco2.imgs.keys())
     dataset_dicts = []
-    for img_id in img_ids: # run through all images
-        record = {} # one record per image
+    for img_id in img_ids:  # run through all images
+        record = {}  # one record per image
         objs = []
-        record["file_name"] = os.path.join("test_images/", coco2.imgs[img_id]['file_name'])
+        record["file_name"] = os.path.join(
+            "test_images/", coco2.imgs[img_id]['file_name'])
         record["width"] = coco2.imgs[img_id]['width']
         record["height"] = coco2.imgs[img_id]['height']
         record["image_id"] = img_id
         annids = coco2.getAnnIds(imgIds=img_id)
         anns = coco2.loadAnns(annids)
-        for i in range(len(annids)): # combine into single annotation format
+        # combine into single annotation format
+        for i in range(len(annids)):
             obj = {
-                "bbox": [0,0,0,0],
+                "bbox": [0, 0, 0, 0],
                 "bbox_mode": BoxMode.XYWH_ABS,
-                "segmentation": [0,0,0,0],
+                "segmentation": [0, 0, 0, 0],
                 "category_id": 0,
                 "iscrowd": 0
             }
@@ -54,65 +57,66 @@ def get_voc_test_dicts(img_dir):
         dataset_dicts.append(record)
     return dataset_dicts
 
+
 # register catlogs for the model
-from detectron2.data import DatasetCatalog, MetadataCatalog
-for d in ["test"]: # total 20 classes, name for visulization purpose(removed)
+for d in ["test"]:  # total 20 classes, name for visulization purpose(removed)
     DatasetCatalog.register("voc_" + d, lambda d=d: get_voc_test_dicts(d))
-    MetadataCatalog.get("voc_" + d).set(thing_classes=["aeroplane",
-    "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair",
-    "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant",
-    "sheep", "sofa", "train", "tvmonitor"])
+    MetadataCatalog.get("voc_" + d).set(thing_classes=[
+        "aeroplane", "bicycle", "bird", "boat", "bottle",
+        "bus", "car", "cat", "chair", "cow",
+        "diningtable", "dog", "horse", "motorbike", "person",
+        "pottedplant", "sheep", "sofa", "train", "tvmonitor"])
 voc_test_metadata = MetadataCatalog.get("voc_train")
 
-from detectron2.engine import DefaultTrainer
-from detectron2.config import get_cfg
-
 ''' set up config file(below) same as training '''
-cfg = get_cfg() # may inherit from file you set
+cfg = get_cfg()  # may inherit from file you set
 cfg.merge_from_file("./configs/MISC/cascade_mask_rcnn_R_50_FPN_3x.yaml")
 cfg.DATASETS.TRAIN = ()
-cfg.DATALOADER.NUM_WORKERS = 0 # not to OOM / break pipe
+cfg.DATALOADER.NUM_WORKERS = 0  # not to OOM / break pipe
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025
 cfg.SOLVER.MAX_ITER = 80000
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
-cfg.MODEL.ROI_HEADS.NUM_CLASSES = 20 # total 20 classes
+cfg.MODEL.ROI_HEADS.NUM_CLASSES = 20  # total 20 classes
 
-os.makedirs(cfg.OUTPUT_DIR, exist_ok=True) # where the weight locate
+os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)  # where the weight locate
 
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
+# set the testing threshold for this model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 cfg.DATASETS.TEST = ("voc_test", )
-dataset_dicts = get_voc_test_dicts("test") # load the test data as set in get_dict
+#  load the test data as set in get_dict
+dataset_dicts = get_voc_test_dicts("test")
 
-train_iter = 50000 
+train_iter = 50000
 ''' set up config file(above) same as training '''
 
 # Run through multiple loops to generate submission files
 # for all check points.
 for i in range(int(train_iter / 5000)):
     cur_iter = ((i + 1) * 5000) - 1
-    if(cur_iter < 10000): # way the checkpoint save
-        cur_name = "model_000" + str(cur_iter) +".pth"
+    if(cur_iter < 10000):  # way the checkpoint save
+        cur_name = "model_000" + str(cur_iter) + ".pth"
     else:
         cur_name = "model_00" + str(cur_iter) + ".pth"
     print(cur_name)
-    cfg.MODEL.WEIGHTS = os.path.join("output/", cur_name) # already 10000
+    cfg.MODEL.WEIGHTS = os.path.join("output/", cur_name)  # already 10000
     predictor = DefaultPredictor(cfg)
     coco_dt = []
-    for d in dataset_dicts: # each dict in single image      
+    for d in dataset_dicts:  # each dict in single image
         im = cv2.imread(d["file_name"])
-        outputs = predictor(im) # get predictiopn
+        outputs = predictor(im)  # get predictiopn
         instance_len = len(outputs["instances"].to("cpu").scores.numpy())
         score_nd = outputs["instances"].to("cpu").scores.numpy()
         class_nd = outputs["instances"].to("cpu").pred_classes.numpy()
         mask_nd = np.asarray(outputs["instances"].to("cpu").pred_masks)
-        for j in range(instance_len): # each segementation for one dictiuonary
+        for j in range(instance_len):  # each segementation for one dictiuonary
             pred = {}
             pred['image_id'] = int(d["image_id"])
-            pred['category_id'] = int(class_nd[j]) + 1 # add back from 0-19 to 1-20
+            # add back from 0-19 to 1-20
+            pred['category_id'] = int(class_nd[j]) + 1
             pred['segmentation'] = binary_mask_to_rle(mask_nd[j])
             pred['score'] = float(score_nd[j])
             coco_dt.append(pred)
-    with open("submission_" + str(i) + ".json", "w") as f: # output multiple predictions 
+    # output multiple predictions
+    with open("submission_" + str(i) + ".json", "w") as f:
         json.dump(coco_dt, f)
-    
--- Detectron2/detectron_train_mod.py
+++ Detectron2/detectron_train_mod.py
@@ -1,95 +1,100 @@
-import torch, torchvision
-torch.__version__
-
+import torch
+import torchvision
 import detectron2
-from detectron2.utils.logger import setup_logger
-setup_logger()
-
-import numpy as np
+import os
+import json
+import itertools
 import cv2
 import random
-
+import numpy as np
 from detectron2.engine import DefaultPredictor
 from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer
 from detectron2.data import MetadataCatalog
-
-import os
-import numpy as np
-import json
+from detectron2.utils.logger import setup_logger
 from detectron2.structures import BoxMode
-import itertools
-
+from detectron2.data import DatasetCatalog
+from detectron2.data import MetadataCatalog
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import ColorMode
 from pycocotools.coco import COCO
 from pycocotools.cocoeval import COCOeval
-coco = COCO("pascal_train.json") # load training annotations
 
-def get_voc_dicts(img_dir): # load train, vallidation data
-    img_ids = list(coco.imgs.keys()) # get all images id in json file
+torch.__version__
+setup_logger()
+coco = COCO("pascal_train.json")  # load training annotations
+
+
+def get_voc_dicts(img_dir):  # load train, vallidation data
+    img_ids = list(coco.imgs.keys())  # get all images id in json file
     dataset_dicts = []
-    for img_id in img_ids: # run through all images
+    for img_id in img_ids:  # run through all images
         record = {}
         objs = []
-        record["file_name"] = os.path.join(img_dir + "/", coco.imgs[img_id]['file_name']) # image direcory
+        record["file_name"] = os.path.join(
+            img_dir + "/", coco.imgs[img_id]['file_name'])  # image direcory
         record["width"] = coco.imgs[img_id]['width']
         record["height"] = coco.imgs[img_id]['height']
         record["image_id"] = img_id
-        annids = coco.getAnnIds(imgIds=img_id) # get mask and bbox information
+        annids = coco.getAnnIds(imgIds=img_id)  # get mask and bbox information
         anns = coco.loadAnns(annids)
         for i in range(len(annids)):
             obj = {
                 "bbox": anns[i]['bbox'],
-                "bbox_mode": BoxMode.XYWH_ABS, # correct format of bounding box for this task
+                # correct format of bounding box for this task
+                "bbox_mode": BoxMode.XYWH_ABS,
                 "segmentation": anns[i]['segmentation'],
-                "category_id": anns[i]['category_id'] - 1, # for remapping to 0-19
+                # for remapping to 0-19
+                "category_id": anns[i]['category_id'] - 1,
                 "iscrowd": 0
             }
             objs.append(obj)
         record["annotations"] = objs
-        dataset_dicts.append(record) # append aone record fopr one image
+        dataset_dicts.append(record)  # append aone record fopr one image
     return dataset_dicts
+
 
 # load dict into catlog
 # also load class name into catlog
-from detectron2.data import DatasetCatalog, MetadataCatalog
 for d in ["train", "val"]:
     DatasetCatalog.register("voc_" + d, lambda d=d: get_voc_dicts(d))
-    MetadataCatalog.get("voc_" + d).set(thing_classes=["aeroplane",
-    "bicycle", "bird", "boat", "bottle", "bus", "car", "cat", "chair",
-    "cow", "diningtable", "dog", "horse", "motorbike", "person", "pottedplant",
-    "sheep", "sofa", "train", "tvmonitor"])
+    MetadataCatalog.get("voc_" + d).set(thing_classes=[
+        "aeroplane", "bicycle", "bird", "boat", "bottle",
+        "bus", "car", "cat", "chair", "cow",
+        "diningtable", "dog", "horse", "motorbike", "person",
+        "pottedplant", "sheep", "sofa", "train", "tvmonitor"])
 voc_metadata = MetadataCatalog.get("voc_train")
 
 dataset_dicts = get_voc_dicts("train")
-for d in random.sample(dataset_dicts, 5): # show 5 image for debugging
+for d in random.sample(dataset_dicts, 5):  # show 5 image for debugging
     img = cv2.imread(d["file_name"])
     visualizer = Visualizer(img[:, :, ::-1], metadata=voc_metadata, scale=0.5)
     vis = visualizer.draw_dataset_dict(d)
-    cv2.imshow("res",(vis.get_image()[:, :, ::-1]))
+    cv2.imshow("res", (vis.get_image()[:, :, ::-1]))
     cv2.waitKey(0)
 
-from detectron2.engine import DefaultTrainer
-from detectron2.config import get_cfg
-
+''' set up config file(below) same as testing '''
 cfg = get_cfg()
 # use cascade mrcnn config file
 cfg.merge_from_file("./configs/MISC/cascade_mask_rcnn_R_50_FPN_3x.yaml")
 cfg.DATASETS.TRAIN = ("voc_train",)
 cfg.DATASETS.TEST = ()
-cfg.DATALOADER.NUM_WORKERS = 0 # not to OOM / break pipe
+cfg.DATALOADER.NUM_WORKERS = 0  # not to OOM / break pipe
 # can also uncomment this line to use default setting
 cfg.MODEL.WEIGHTS = "detectron2://ImageNetPretrained/MSRA/R-50.pkl"
 # uncomment to train on your own pretrain
 # cfg.MODEL.WEIGHTS = "./output/model_final.pth"
 cfg.SOLVER.IMS_PER_BATCH = 2
 cfg.SOLVER.BASE_LR = 0.00025
-cfg.SOLVER.MAX_ITER = 50000 # how much iteration you want to run
+cfg.SOLVER.MAX_ITER = 50000  # how much iteration you want to run
 cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128
 cfg.MODEL.ROI_HEADS.NUM_CLASSES = 20
+''' set up config file(above) same as testing '''
 
 os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
-trainer = DefaultTrainer(cfg) 
-trainer.resume_or_load(resume=False) # reload
+trainer = DefaultTrainer(cfg)
+trainer.resume_or_load(resume=False)  # reload
 trainer.train()
 
 # Look at training curves in tensorboard:
@@ -97,21 +102,23 @@ trainer.train()
 
 ''' model may be save in output directory '''
 cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
-cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5   # set the testing threshold for this model
+# set the testing threshold for this model
+cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
 cfg.DATASETS.TEST = ("voc_val", )
 predictor = DefaultPredictor(cfg)
 
 ''' visualize validation datas '''
-from detectron2.utils.visualizer import ColorMode
-dataset_dicts = get_voc_dicts("val") # tewt at validation set (currently same as train) 
-for d in random.sample(dataset_dicts, 5):    
+# tewt at validation set (currently same as train)
+dataset_dicts = get_voc_dicts("val")
+for d in random.sample(dataset_dicts, 5):
     im = cv2.imread(d["file_name"])
     outputs = predictor(im)
     v = Visualizer(im[:, :, ::-1],
-                   metadata=voc_metadata, 
-                   scale=0.8, 
-                   instance_mode=ColorMode.IMAGE_BW   # remove colors for unsegments
-    )
+                   metadata=voc_metadata,
+                   scale=0.8,
+                   # remove colors for unsegments
+                   instance_mode=ColorMode.IMAGE_BW
+                   )
     v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
     cv2.imshow("result", (v.get_image()[:, :, ::-1]))
     cv2.waitKey(0)
